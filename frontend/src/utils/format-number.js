ba37d1a0060e6445ea9eb3836cacf7d98969314d
Adiazi-ugochukwu-precious <ortezzyadiazi@gmail.com>
2025-08-05 02:23:24 +0100

    Remove all frontend folders completely

--- frontend/src/utils/format-number.js
+++
@@ -1,7 +0,0 @@
-export const formatNumber = (num) => {
-    return num.toLocaleString();
-};
-
-export const formatNumberTONaira = (num) => {
-    return `₦${num.toLocaleString()}`;
-};
--- frontend_backup/src/utils/TextEncoder.js
+++
@@ -1,6 +0,0 @@
-import { hiveIn } from './hive';
-
-export const encode = (data) => {
-    const encoder = new TextEncoder();
-    return encoder.encode(hiveIn(data));
-};
